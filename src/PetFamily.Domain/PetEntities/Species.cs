b8502ad183a664a39d8a7bc884fb635a72c6dd14
fuser85 <fuser85@myoffice365.site>
2025-07-02 21:48:30 +0300

    + Entity Species

---
+++ src/PetFamily.Domain/PetEntities/Species.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.PetEntities;
+
+public class Species : Entity<Guid>
+{
+	public Species(Guid id, string name) : base(id)
+	{
+		Name = name;
+	}
+
+	public string Name { get; set; }
+	public IReadOnlyList<Breed> Breeds { get; set; } = [];
+
+
+	public static Guid NewId() => Guid.NewGuid();
+
+	public static Result<Species> Create(string name)
+	{
+
+		if (string.IsNullOrWhiteSpace(name))
+			return Result.Failure<Species>("Name cannot be empty");
+
+		var species = new Species(NewId(), name);
+
+		return Result.Success(species);
+	}
+}
