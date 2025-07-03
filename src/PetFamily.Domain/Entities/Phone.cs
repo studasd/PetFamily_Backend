65f1bb51c54957630648264e5b09d9f42eb6a209
fuser85 <fuser85@myoffice365.site>
2025-07-03 13:14:38 +0300

    + Check phone number

--- src/PetFamily.Domain/Entities/Phone.cs
+++ src/PetFamily.Domain/Entities/Phone.cs
@@ -1,4 +1,5 @@
-﻿using CSharpFunctionalExtensions;
+﻿using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
 
 namespace PetFamily.Domain.Entities;
 
@@ -8,6 +9,10 @@ public record Phone(string phone)
 	{
 		if (string.IsNullOrWhiteSpace(phone))
 			return Result.Failure<Phone>("Phone cannot be empty");
+
+		var checkPhone = Regex.Match(phone, @"^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$");
+		if (!checkPhone.Success)
+			return Result.Failure<Phone>("Error in the phone number format");
 
 		return new Phone(phone);
 	}
