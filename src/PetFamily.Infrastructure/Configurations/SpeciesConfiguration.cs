040b7809245c5351da0f56e8bf19282930df0394
fuser85 <fuser85@myoffice365.site>
2025-07-04 01:12:01 +0300

    + config Species

---
+++ src/PetFamily.Infrastructure/Configurations/SpeciesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.PetEntities;
+using PetFamily.Infrastructure.Shared;
+
+namespace PetFamily.Infrastructure.Configurations;
+
+internal class SpeciesConfiguration : IEntityTypeConfiguration<Species>
+{
+	public void Configure(EntityTypeBuilder<Species> builder)
+	{
+		builder.ToTable("species");
+
+		builder.HasKey(p => p.Id);
+
+		builder.Property(p => p.Name)
+			.IsRequired()
+			.HasMaxLength(Constants.MAX_LOW_TEXT_LENGHT);
+
+		builder.HasMany(p => p.Breeds)
+			.WithOne()
+			.HasForeignKey("breed_id")
+			.IsRequired()
+			.OnDelete(DeleteBehavior.NoAction);
+	}
+}
