9f85d1dfa9e808757eeaf1a9b2cdf5a2dbbcb06e
fuser85 <fuser85@myoffice365.site>
2025-07-03 17:51:22 +0300

    + connection string

--- src/PetFamily.Infrastructure/ApplicationDbContext.cs
+++ src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PetFamily.Domain.VolunteerEntities;
 
 namespace PetFamily.Infrastructure;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext(IConfiguration configuration) : DbContext
 {
+	private const string DATABASE = "Database";
+
+
 	public DbSet<Volunteer> Volunteers => Set<Volunteer>();
+
 
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
-		optionsBuilder.UseNpgsql();
+		optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
 	}
 }
