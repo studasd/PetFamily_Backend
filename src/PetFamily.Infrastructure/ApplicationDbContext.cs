23b9d65b8607407e621d1b156dfca625ac614e44
fuser85 <fuser85@myoffice365.site>
2025-07-03 16:26:07 +0300

    + DbContext

---
+++ src/PetFamily.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetFamily.Domain.VolunteerEntities;
+
+namespace PetFamily.Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+	public DbSet<Volunteer> Volunteers => Set<Volunteer>();
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseNpgsql();
+	}
+}
