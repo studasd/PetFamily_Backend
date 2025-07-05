c031b19fda69cf65c3620081546002cb91bb5ed3
fuser85 <fuser85@myoffice365.site>
2025-07-05 03:13:43 +0300

    + Add controllers

---
+++ src/PetFamily.API/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetFamily.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+
+	[HttpGet]
+	public async Task<IActionResult> Add()
+	{
+		return StatusCode(200);
+	}
+}
--- src/PetFamily.API/Program.cs
+++ src/PetFamily.API/Program.cs
@@ -2,10 +2,25 @@ using PetFamily.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Add services to the container.
 builder.Services.AddScoped<ApplicationDbContext>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
 
 // Configure the HTTP request pipeline.
 
