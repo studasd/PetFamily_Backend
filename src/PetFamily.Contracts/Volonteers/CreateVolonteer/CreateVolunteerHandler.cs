1164094810e86fd06000456853374741fda16483
fuser85 <fuser85@myoffice365.site>
2025-07-05 15:08:46 +0300

    + Add CreateVolunteer Request/Handler

---
+++ src/PetFamily.Contracts/Volonteers/CreateVolonteer/CreateVolunteerHandler.cs
@@ -0,0 +1,28 @@
+﻿
+using CSharpFunctionalExtensions;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.VolunteerEntities;
+
+namespace PetFamily.Contracts.Volonteers.CreateVolonteer;
+
+public class CreateVolunteerHandler // CreateVolunteerService
+{
+	private readonly IVolunteerRepository volunteerRepository;
+
+	public CreateVolunteerHandler(IVolunteerRepository volunteerRepository)
+	{
+		this.volunteerRepository = volunteerRepository;
+	}
+
+	public async Task<Result<Guid, Error>> HandleAsync(CreateVolunteerRequest request, CancellationToken token = default)
+	{
+		var volunteer = Volunteer.Create(request.Firstname, request.Lastname, request.Surname, request.Email, request.Description, request.ExperienceYears, request.Phone);
+
+		if (volunteer.IsFailure)
+			return volunteer.Error;
+
+		await volunteerRepository.AddAsync(volunteer.Value, token);
+
+		return volunteer.Value.Id.Value;
+	}
+}
---
+++ src/PetFamily.Contracts/Volonteers/CreateVolonteer/CreateVolunteerRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFamily.Contracts.Volonteers.CreateVolonteer;
+
+public record CreateVolunteerRequest(string Firstname, string Lastname, string Surname, string Email, string Description, int ExperienceYears, string Phone);
