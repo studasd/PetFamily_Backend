1e9b6c2ce848a3801fdcce42f315c99610cb4130
fuser85 <fuser85@myoffice365.site>
2025-07-05 04:29:52 +0300

    + Controller test volonteer

--- src/PetFamily.API/Controllers/HomeController.cs
+++ src/PetFamily.API/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PetFamily.Domain.VolunteerEntities;
 
 namespace PetFamily.API.Controllers;
 
@@ -10,6 +11,11 @@ public class HomeController : ControllerBase
 	[HttpGet]
 	public async Task<IActionResult> Add()
 	{
-		return StatusCode(200);
+		var volonteer = Volunteer.Create("firsname", "lastname", "surname", "email@email.mm", "description", 1, "7777777777");
+
+		if(volonteer.IsFailure)
+			return StatusCode(volonteer.Error.TypeCode, volonteer.Error);
+
+		return Ok(volonteer.Value);
 	}
 }
