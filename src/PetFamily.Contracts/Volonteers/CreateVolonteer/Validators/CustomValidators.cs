11f59e213c313cf646bcc13209365467b07430f8
fuser85 <fuser85@myoffice365.site>
2025-07-07 02:36:53 +0300

    + Add CustomValidators from MustBeValueObject

---
+++ src/PetFamily.Contracts/Volonteers/CreateVolonteer/Validators/CustomValidators.cs
@@ -0,0 +1,22 @@
+﻿using CSharpFunctionalExtensions;
+using FluentValidation;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Contracts.Volonteers.CreateVolonteer.Validators;
+
+public static class CustomValidators
+{
+	// Валидация ValueObjects 6-1.43.20
+	public static IRuleBuilderOptionsConditions<T, TElement> MustBeValueObject<T, TElement, TValueObject>(this IRuleBuilder<T, TElement> ruleBuilder, Func<TElement, Result<TValueObject, Error>> factoryMethod)
+	{
+		return ruleBuilder.Custom((value, context) =>
+		{
+			Result<TValueObject, Error> result = factoryMethod(value);
+
+			if (result.IsSuccess)
+				return;
+
+			context.AddFailure(result.Error.Message);
+		});
+	}
+}
