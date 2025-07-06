8daff2565a2ed34c3eb3265919a75eaa2a1477d1
fuser85 <fuser85@myoffice365.site>
2025-07-06 20:37:49 +0300

    + Add Envelop

---
+++ src/PetFamily.API/Responses/Envelop.cs
@@ -0,0 +1,23 @@
+﻿namespace PetFamily.API.Responses;
+
+public record ResponseError(string? ErrorCode, string? ErrorMessage, string? InvalidField);
+
+
+public record Envelop
+{
+	public object? Result { get; }
+	public List<ResponseError> Errors { get; }
+	public DateTime TimeGenerated { get; }
+
+	public Envelop(object? result, IEnumerable<ResponseError> errors)
+	{
+		Result = result;
+		Errors = errors.ToList();
+		TimeGenerated = DateTime.Now;
+	}
+
+
+	public static Envelop Ok(object? result = null) => new(result, []);
+	
+	public static Envelop Error(IEnumerable<ResponseError> errors) => new(null, errors);
+}
