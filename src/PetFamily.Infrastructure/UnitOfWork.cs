92c7d36980b004ed3f54d691e03a5615ca086c14
fuser85 <fuser85@myoffice365.site>
2025-07-15 02:41:45 +0300

    + Add unitofwork

---
+++ src/PetFamily.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using PetFamily.Application.Database;
+using System.Data;
+
+namespace PetFamily.Infrastructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+	private readonly ApplicationDbContext db;
+
+	public UnitOfWork(ApplicationDbContext db)
+	{
+		this.db = db;
+	}
+
+
+	public async Task<IDbTransaction> BeginTransactionAsync(CancellationToken token)
+	{
+		var transaction = await db.Database.BeginTransactionAsync(token);
+
+		return transaction.GetDbTransaction();
+	}
+
+	public async Task SaveChangesAsync(CancellationToken token)
+	{
+		await db.SaveChangesAsync(token);
+	}
+}
